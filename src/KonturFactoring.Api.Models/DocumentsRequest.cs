4580820e00342f6add7b745b0bc1c2608fa0c1ea
EatsLemons <frimko@gmail.com>
2019-09-18 14:00:37 +0300

    update documents req/resp

--- src/KonturFactoring.Api.Models/DocumentsRequest.cs
+++ src/KonturFactoring.Api.Models/DocumentsRequest.cs
@@ -1,16 +1,21 @@
 using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace KonturFactoring.Api.Models
 {
     public class DocumentsRequest
     {
+        [JsonProperty("afterKey")]
         public int AfterKey { get; set; }
 
+        [JsonProperty("count")]
         public int Count { get; set; }
 
+        [JsonProperty("fromDate")]
         public DateTime? FromDate { get; set; }
 
+        [JsonProperty("organizationIds")]
         public List<int> OrganizationsIds { get; set; }
     }
 }
--- src/KonturFactoring.Api.Models/DocumentsResponse.cs
+++ src/KonturFactoring.Api.Models/DocumentsResponse.cs
@@ -1,4 +1,5 @@
 using System;
+using Newtonsoft.Json;
 
 namespace KonturFactoring.Api.Models
 {
@@ -7,66 +8,91 @@ namespace KonturFactoring.Api.Models
     /// </summary>
     public class DocumentsResponse
     {
+        [JsonProperty("id")]
         public int Id { get; set; }
 
+        [JsonProperty("createdAt")]
         public DateTime CreatedAt { get; set; }
 
+        [JsonProperty("type")]
         public string Type { get; set; }
 
+        [JsonProperty("number")]
         public string Number { get; set; }
 
+        [JsonProperty("date")]
         public DateTime Date { get; set; }
         
+        [JsonProperty("totalSumWithVat")]
         public decimal TotalSumWithVat { get; set; }
         
+        [JsonProperty("status")]
         public string Status { get; set; }
 
+        [JsonProperty("seller")]
         public CompanyInfo Seller { get; set; }
 
+        [JsonProperty("buyer")]
         public CompanyInfo Buyer { get; set; }
 
+        [JsonProperty("relations")]
         public Relations Relations { get; set; }
 
+        [JsonProperty("mustBeConfirmed")]
         public bool MustBeConfirmed { get; set; }
 
+        [JsonProperty("confirmationInfo")]
         public ConfirmationInfo ConfirmationInfo { get; set; }
 
+        [JsonProperty("afterKey")]
         public long AfterKey { get; set; }
     }
 
     public class ConfirmationInfo
     {
+        [JsonProperty("confirmed")]
         public bool Confirmed { get; set; }
 
+        [JsonProperty("amount")]
         public int Amount { get; set; }
 
+        [JsonProperty("delayStartDate")]
         public DateTime DelayStartDate { get; set; }
     }
 
     public class Relations
     {
+        [JsonProperty("organizationIds")]
         public RelationDocument DeliveryAgreement { get; set; }
 
+        [JsonProperty("organizationIds")]
         public RelationDocument FactoringAgreement { get; set; }
 
+        [JsonProperty("organizationIds")]
         public RelationDocument BaseDocument { get; set; }
     }
 
     public class RelationDocument
     {
-        public string Type { get; set; }
+        [JsonProperty("date")]
+        public DateTime Date { get; set; }
 
+        [JsonProperty("number")]
         public string Number { get; set; }
     }
 
     public class CompanyInfo
     {
+        [JsonProperty("id")]
         public int Id { get; set; }
 
+        [JsonProperty("inn")]
         public string Inn { get; set; }
 
+        [JsonProperty("kpp")]
         public string Kpp { get; set; }
 
+        [JsonProperty("name")]
         public string Name { get; set; }
     }
 }
